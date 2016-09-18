d2ed5be8cd57c1a8ebc7c248ce0f08cbfdf3fe0b
Andrew Pogrebnjak <fashust.nefor@gmail.com>
2016-09-18 12:52:36 +0300

    remove

--- converter_thread.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*- #
-"""
-    Created on 18.09.16 by fashust
-"""
-from __future__ import absolute_import, print_function, unicode_literals
-
-
-__author__ = 'fashust'
-__email__ = 'fashust.nefor@gmail.com'
