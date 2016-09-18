965c99c07bc64d2cded718d4b44d4b88804dcf54
Andrew Pogrebnjak <fashust.nefor@gmail.com>
2016-09-18 10:31:35 +0300

    convert without deleting

--- converter.py
+++ converter.py
@@ -2,16 +2,143 @@
 """
     Created on 18.09.16 by fashust
 """
+import os
+import asyncio
+import shlex
+from subprocess import call
 
 
 __author__ = 'fashust'
 __email__ = 'fashust.nefor@gmail.com'
 
 
-def convert(cource_dir, ffmpeg_bin):
+EXTENTION = '.cue'
+
+
+def get_cue_file(source_dir):
+    """
+    find cue file in source dir
+    :param source_dir:
+    :raise Exception
+    :return:
+    """
+    for _file in os.listdir(source_dir):
+        if _file.endswith(EXTENTION):
+            return os.path.abspath(os.path.join(source_dir, _file))
+    msg = '*.cue file not found in directory "{}"'.format(source_dir)
+    raise Exception(msg)
+
+
+def generate_commands(cue_file, ffmpeg_bin):
+    """
+    generate convert commands from cue file
+    based on gist forked from
+    https://gist.github.com/bancek/b37b780292540ed2d17d
+    :param cue_file:
+    :return:
+    """
+    general = {}
+    tracks = []
+    commands = []
+    with open(cue_file) as _cue_file:
+        file_data = _cue_file.read().splitlines()
+        current_file = None
+        for line in file_data:
+            if line.startswith('REM GENRE '):
+                general['genre'] = ' '.join(line.split(' ')[2:])
+            if line.startswith('REM DATE '):
+                general['date'] = ' '.join(line.split(' ')[2:])
+            if line.startswith('PERFORMER '):
+                general['artist'] = ' '.join(
+                    line.split(' ')[1:]
+                ).replace('"', '')
+            if line.startswith('TITLE '):
+                general['album'] = ' '.join(
+                    line.split(' ')[1:]
+                ).replace('"', '')
+            if line.startswith('FILE '):
+                current_file = ' '.join(line.split(' ')[1:-1]).replace('"', '')
+            if line.startswith('  TRACK '):
+                track = general.copy()
+                track['track'] = int(line.strip().split(' ')[1], 10)
+                tracks.append(track)
+            if line.startswith('    TITLE '):
+                tracks[-1]['title'] = ' '.join(
+                    line.strip().split(' ')[1:]).replace('"', '')
+            if line.startswith('    PERFORMER '):
+                tracks[-1]['artist'] = ' '.join(
+                    line.strip().split(' ')[1:]).replace('"', '')
+            if line.startswith('    INDEX 01 '):
+                t = list(map(
+                    int,
+                    ' '.join(
+                        line.strip().split(' ')[2:]
+                    ).replace('"', '').split(':')
+                ))
+                tracks[-1]['start'] = 60 * t[0] + t[1] + t[2] / 100.0
+        for i in range(len(tracks)):
+            if i != len(tracks) - 1:
+                tracks[i]['duration'] = (
+                    tracks[i + 1]['start'] - tracks[i]['start']
+                )
+        for track in tracks:
+            metadata = {
+                'artist': track['artist'],
+                'title': track['title'],
+                'album': track['album'],
+                'track': str(track['track']) + '/' + str(len(tracks))
+            }
+
+            if 'genre' in track:
+                metadata['genre'] = track['genre']
+            if 'date' in track:
+                metadata['date'] = track['date']
+
+            cmd = ffmpeg_bin
+            cmd += ' -b:a 320k'
+            cmd += ' -i "%s"' % current_file
+            cmd += ' -ss %.2d:%.2d:%.2d' % (
+                track['start'] / 60 / 60, track['start'] / 60 % 60,
+                int(track['start'] % 60)
+            )
+
+            if 'duration' in track:
+                cmd += ' -t %.2d:%.2d:%.2d' % (
+                    track['duration'] / 60 / 60, track['duration'] / 60 % 60,
+                    int(track['duration'] % 60)
+                )
+
+            cmd += ' ' + ' '.join(
+                '-metadata %s="%s"' % (k, v) for (k, v) in metadata.items()
+            )
+            cmd += ' "%.2d - %s - %s.mp3"' % (
+                track['track'], track['artist'], track['title']
+            )
+            commands.append(cmd)
+    return commands
+
+
+async def run_command(command, source_dir):
+    """
+    excute convert command
+    :param source_dir:
+    :param command:
+    :return:
+    """
+    command = command.replace('""', '"')
+    os.chdir(os.path.abspath(source_dir))
+    call(shlex.split(command))
+
+
+def convert(source_dir, ffmpeg_bin):
     """
     converter
     """
+    cue_file = get_cue_file(source_dir)
+    commands = generate_commands(cue_file, ffmpeg_bin)
+    loop = asyncio.get_event_loop()
+    for command in commands:
+        loop.run_until_complete(run_command(command, source_dir))
 
 
 def delete_original(source_dir):
